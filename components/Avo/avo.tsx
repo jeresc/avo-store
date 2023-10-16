0d97d8931f80c6deb96509e475cb2a0364e3180b
jeresc <jeremiastomassrc@gmail.com>
2023-10-15 23:07:02 -0300

    feat: ✨ add Link to Avo detail page

--- components/Avo/avo.tsx
+++ components/Avo/avo.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 interface IProps {
@@ -12,7 +13,9 @@ export function Avo({ avo }: IProps) {
       rounded-2xl shadow-lg p-4 gap-2 max-w-sm hover:scale-105 duration-200 transition-all
       hover:shadow-xl cursor-pointer"
     >
-      <Image src={avo.image} alt={avo.name} width={256} height={256} />
+      <Link href={`/product/${avo.id}`}>
+        <Image src={avo.image} alt={avo.name} width={256} height={256} />
+      </Link>
       <span className="flex gap-2 justify-center flex-wrap">
         <h2 className="text-xl font-medium">{avo.name}</h2>
         <p className="bg-lime-200 p-0.5 rounded-md px-1.5 text-lime-600 font-bold">
