1652d674f9db977e44356484df4b03d43c69ae11
jeresc <jeremiastomassrc@gmail.com>
2023-10-16 00:02:58 -0300

    feat: ⚡ add SSG on Avo Detail

--- pages/product/[productId].tsx
+++ pages/product/[productId].tsx
@@ -1,5 +1,6 @@
 import { useRouter } from 'next/router'
 import React, { useEffect, useState } from 'react'
+import DB from '@database'
 
 function ProductItem() {
   const { productId } = useRouter().query
@@ -7,14 +8,51 @@ function ProductItem() {
 
   useEffect(() => {
     if (typeof productId !== 'string') return
+
     window
       .fetch(`/api/avo/${productId}`)
       .then(async (res) => await res.json())
       .then(setProduct)
       .catch(console.error)
+
+    return () => {
+      setProduct(undefined)
+    }
   }, [productId])
 
   return <div>{product?.name}</div>
 }
 
 export default ProductItem
+
+export async function getStaticPaths() {
+  const db = new DB()
+  const products = await db.getAll()
+  const paths = products.map((product) => ({
+    params: {
+      productId: product.id
+    }
+  }))
+
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+interface IParams {
+  params: {
+    productId: string
+  }
+}
+
+export async function getStaticProps({ params }: IParams) {
+  const db = new DB()
+  const product = await db.getById(params.productId)
+
+  return {
+    props: {
+      product
+    }
+  }
+}
